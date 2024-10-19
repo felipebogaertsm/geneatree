9a251016f8e95a47b70bd8cd6e65e443b6bb16e6
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2024-10-19 18:17:32 -0300

    Removed admin fields

--- api/apps/genealogy/admin.py
+++ api/apps/genealogy/admin.py
@@ -1,23 +1,13 @@
 from django.contrib import admin
 
-from .models import FamilyMember, Union, ParentChild
+from .models import FamilyMember, Union
 
 
 @admin.register(FamilyMember)
 class FamilyMemberAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "birth", "death")
+    pass
 
 
 @admin.register(Union)
 class UnionAdmin(admin.ModelAdmin):
-    list_display = (
-        "parent_1",
-        "parent_2",
-        "date_of_marriage",
-        "date_of_divorce",
-    )
-
-
-@admin.register(ParentChild)
-class ParentChildAdmin(admin.ModelAdmin):
-    list_display = ("parent", "child", "union")
+    pass
